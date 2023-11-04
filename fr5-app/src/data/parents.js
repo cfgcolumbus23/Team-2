42cca57e9f2678fe503acde0ae2309d270236a01
PersonalFleek <personalfleek@gmail.com>
2023-11-04 11:13:01 -0400

    Login form added

--- fr5-app/src/data/parents.js
+++ fr5-app/src/data/parents.js
@@ -1,15 +1,14 @@
-// Mock data for parents
 const parents = [
-  { id: 1, name: 'Ava Smith', email: 'ava@kinderschool.org' },
-  { id: 2, name: 'Liam Johnson', email: 'liam@littlelearners.edu' },
-  { id: 3, name: 'Olivia Williams', email: 'olivia@prepstart.academy' },
-  { id: 4, name: 'Noah Brown', email: 'noah@earlysteps.net' },
-  { id: 5, name: 'Emma Jones', email: 'emma@brightbeginnings.co' },
-  { id: 6, name: 'Oliver Garcia', email: 'oliver@youngminds.org' },
-  { id: 7, name: 'Amelia Davis', email: 'amelia@firstclass.edu' },
-  { id: 8, name: 'Elijah Martinez', email: 'elijah@startingsmart.org' },
-  { id: 9, name: 'Isabella Rodriguez', email: 'isabella@newhorizons.school' },
-  { id: 10, name: 'Lucas Hernandez', email: 'lucas@futuregrads.academy' }
+  { id: 1, type: "parent", name: "Ava Smith", email: "ava@kinderschool.org", password: "password123", profileImage: "https://placeholder.com/parent1.jpg" },
+  { id: 2, type: "parent", name: "Liam Johnson", email: "liam@littlelearners.edu", password: "password123", profileImage: "https://placeholder.com/parent2.jpg" },
+  { id: 3, type: "parent", name: "Olivia Williams", email: "olivia@prepstart.academy", password: "password123", profileImage: "https://placeholder.com/parent3.jpg" },
+  { id: 4, type: "parent", name: "Noah Brown", email: "noah@earlysteps.net", password: "password123", profileImage: "https://placeholder.com/parent4.jpg" },
+  { id: 5, type: "parent", name: "Emma Jones", email: "emma@brightbeginnings.co", password: "password123", profileImage: "https://placeholder.com/parent5.jpg" },
+  { id: 6, type: "parent", name: "Oliver Garcia", email: "oliver@youngminds.org", password: "password123", profileImage: "https://placeholder.com/parent6.jpg" },
+  { id: 7, type: "parent", name: "Amelia Davis", email: "amelia@firstclass.edu", password: "password123", profileImage: "https://placeholder.com/parent7.jpg" },
+  { id: 8, type: "parent", name: "Elijah Martinez", email: "elijah@startingsmart.org", password: "password123", profileImage: "https://placeholder.com/parent8.jpg" },
+  { id: 9, type: "parent", name: "Isabella Rodriguez", email: "isabella@newhorizons.school", password: "password123", profileImage: "https://placeholder.com/parent9.jpg" },
+  { id: 10, type: "parent", name: "Lucas Hernandez", email: "lucas@futuregrads.academy", password: "password123", profileImage: "https://placeholder.com/parent10.jpg" }
 ];
 
 export default parents;
